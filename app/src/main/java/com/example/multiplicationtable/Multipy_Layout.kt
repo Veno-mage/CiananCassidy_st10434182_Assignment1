624deed278ca21c14f204ef64cbf83e138612f96
Veno-mage <st10434182@vcconnect.edu.za>
2024-03-19 15:53:32 +0200

    finished project

---
+++ app/src/main/java/com/example/multiplicationtable/Multipy_Layout.kt
@@ -0,0 +1,11 @@
+package com.example.multiplicationtable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Multipy_Layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multiplication_tables)
+    }
+}
